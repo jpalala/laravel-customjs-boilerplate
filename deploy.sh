48883ab5414dd2eae6edef1df52f3b7144bb528a
Joe Palala <jose.palala@live.com>
2022-06-15 14:11:10 +0800

    document: add note about the last line

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,5 @@ git clean -f
 # append / include code in welcome file
 # don't git commit the welcome file! 
 # so on every git pull you can re-run this file without being appended again
-echo "\n include_once(__DIR__ . '../relative/path/public/index.phtml');" >> $WELCOME_FILE
-
+# Only uncomment the following line if you don't have the code above in your welcome file (the first view page for the laravel page)
+# echo "@php \n include_once(__DIR__ . '../relative/path/public/index.phtml'); \n @endphp" >> $WELCOME_FILE
