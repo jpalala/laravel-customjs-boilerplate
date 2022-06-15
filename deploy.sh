203f1aaf01ab65d679802f8f9a05f61d7b4e566d
Joe Palala <jose.palala@live.com>
2022-06-15 14:00:57 +0800

    feat: updates to deploy file and default files when deploying

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,12 @@ cp dist/index.html "public/$FRONTEND_FILE"
 # create public folders for css and js:
 mkdir public/css && cp -Rf dist/css/* public/css
 mkdir public/js && cp -Rf dist/js/* public/js
-# append the code to 
+
+# do a git clean
+git clean -f
+
+# append / include code in welcome file
+# don't git commit the welcome file! 
+# so on every git pull you can re-run this file without being appended again
 echo "\n include_once(__DIR__ . '../relative/path/public/index.phtml');" >> $WELCOME_FILE
 
