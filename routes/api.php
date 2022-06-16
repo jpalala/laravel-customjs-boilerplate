d2b3c2cb10a44703521ee3534670bd732551b403
Joe Palala <jose.palala@live.com>
2022-06-16 14:34:04 +0800

    add api login logout

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//public
+Route::post('register', 'Api\\AuthController@register');
+Route::post('login', 'Api\\AuthController@login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
