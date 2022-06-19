48681ca52d288d4369762a36954c4aad5249776a
Joe Palala <jose.palala@live.com>
2022-06-19 18:00:27 +1000

    fix: add cookies on view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,16 @@
 @php
+  function get_cookie($key) {
+    return $_COOKIE[$key];
+  } 
+  $cookie_value = "John Doe";
+  setcookie("user", $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
  $website_title = $title;
+ 
+  if(isset($_COOKIE['user'])) {
+    echo '<script> var username = \'' .$_COOKIE['user']. '\';let x = document.cookie;
+     alert(document.cookie);</script>';
+ }
+
  include_once(base_path() . '/public/index.phtml');
 @endphp
