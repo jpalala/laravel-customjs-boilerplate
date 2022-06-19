8c5fe0a274f24619a76300ef3e01038345af26d5
Joe Palala <jose.palala@live.com>
2022-06-19 19:32:01 +1000

    test new code

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,29 @@
+@php
+  //set a cookie
+  $cookie_value = "John Doe";
+  setcookie("user", $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+  $subtitle = 'Dashboard';
+@endphp
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Svelte + Laravel: 
+          @php 
+              echo $subtitle
+          @endphp
+        </title>
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+        </style>
+        <script> var page = 'dashboard';</script>
+        <script type="module" crossorigin src="/assets/index.f0fac1ad.js"></script>
+        <link rel="stylesheet" href="/assets/index.2362315a.css">
+    </head>
+    <body class="antialiased">
+            <div id="app"></div>
+    </body>
+    </html> 
